bc6d1125e3e4d828d31c2f781b4c3b33d26a666a
blacksidev98 <xc13279112887@gmail.com>
2020-08-18 01:29:18 +0800

    第二晚提交

--- src/views/Movie/index.vue
+++ src/views/Movie/index.vue
@@ -28,20 +28,70 @@ import Header from '@/components/Header'
 import Nowplaying from '@/views/Movie/nowplaying'
 import Comingsoon from '@/views/Movie/comingsoon'
 import City from '@/views/Movie/city'
+
+import { messageBox } from '@/components/JS'
+import Axios from 'axios'
 export default {
   data() {
     return {
+      datalist: [],
+      myIp: ''
     }
   },
   mounted() {
-   
+
+    Axios({
+      url: 'https://ip.seeip.org/geoip'
+    }).then(res => {
+      this.myIp = res.data.ip
+      console.log(this.myIp)
+    })
+    Axios({
+      url: 'https://m.maizuo.com/gateway?k=9325855',
+      headers: {
+        'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"1596954681169569603813377","bc":"210300"}',
+        'X-Host': 'mall.film-ticket.city.list'
+      }
+    }).then(res => {
+      console.log(res.data.data.cities)
+      this.datalist = res.data.data.cities
+    })
+    setTimeout(() => {
+      Axios({
+        url: `http://restapi.amap.com/v3/ip?key=e573dc1792435fe0975dc1e484af680c&ip=${this.myIp}`
+      }).then(res => {
+        var cityName = res.data.city
+        for(var i=0; i<this.datalist.length; i++){
+          if(this.datalist[i].name + '市' === cityName){
+            var cityId = this.datalist[i].cityId
+          }
+        }
+        if( this.$store.state.cityName === cityName){return}
+        messageBox({
+         title: '定位',
+         content: cityName,
+         cancel: '取消',
+         ok: '切换定位',
+         handleCancel(){
+           console.log('cancel')
+         },
+         handleOk(){
+           // 修改状态管理和本地存储
+           localStorage.setItem('id', cityId)
+           localStorage.setItem('name', cityName)
+           location.reload()
+         }
+        })
+      })
+    },2000)
+// https://restapi.amap.com/v3/ip?ip=114.247.50.2&output=xml&key=e573dc1792435fe0975dc1e484af680c
   },
   components: {
     Tabbar,
     Header,
     Nowplaying,
     Comingsoon,
-    City
+    City,
   } 
 }
 </script>
